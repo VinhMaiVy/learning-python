ac33089b4517904028aee9e513eef8bb54295796
Vinh Nguyen <vinhmaivy@gmail.com>
2020-07-12 17:46:45 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='learning-python',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='vinhm',
+    author_email='',
+    description=''
+)

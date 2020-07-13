0802e8d515580a4748ba8deaf02baa36cf6a4f8e
Vinh Nguyen <vinhmaivy@gmail.com>
2020-07-13 06:32:00 -0500

    Initial commit

--- py_if_else.py
+++ py_if_else.py
@@ -1,4 +1,27 @@
-#!/bin/python3
+#!/bin/python
+
+"""
+Task
+
+Given an integer n, perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+Input Format
+A single line containing a positive integer, n.
+
+Constraints
+ 1 <= n <=100
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input 0
+"""
+
 
 import math
 import os
@@ -7,8 +30,19 @@ import re
 import sys
 
 
+def main():
+    if n % 2:
+        print("Weird")
+    else:
+        if (n >= 6) and (n<=20):
+            print("Weird")
+        else:
+            print("Not Weird")
 
 if __name__ == '__main__':
     n = int(input().strip())
 
-print('hello')
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+

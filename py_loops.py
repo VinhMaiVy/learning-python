8b4cba852905a57ca3ad4820b2aa394d6d3404df
Vinh Nguyen <vinhmaivy@gmail.com>
2020-07-16 06:09:55 -0500

    Initial commit

---
+++ py_loops.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+"""
+Task
+The provided code stub reads and integer,n , from STDIN.
+    For all non-negative integers i<n, print i^2.
+
+Example
+    n = 3
+
+Print the following:
+    0
+    1
+    4
+
+"""
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def main():
+    for i in range(n):
+        print(i**2)
+
+if __name__ == '__main__':
+    n = int(input())
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+

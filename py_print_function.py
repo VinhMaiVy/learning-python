ad328c7622d86eda15b3aa1b344b373bba248253
Vinh Nguyen <vinhmaivy@gmail.com>
2020-07-18 08:32:51 -0500

    Initial commit

---
+++ py_print_function.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+"""
+
+The included code stub will read an integer, n , from STDIN.
+
+Without using any string methods, try to print the following:
+
+123...n
+
+Note that "..." represents the consecutive values in between.
+
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+def main():
+    print(*range(1,n+1), sep="")
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()

286b8af26fa91788771d68916e89e3f6d80ea040
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-03 06:30:25 -0500

    Initial commit

---
+++ py_findind_percentage.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+"""
+
+
+
+"""
+
+def main():
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()

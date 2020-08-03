ca403863a9a21c5e786813558334e9d61a3d3d58
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-03 07:03:33 -0500

    Initial commit

--- py_findind_percentage.py
+++ py_findind_percentage.py
@@ -2,13 +2,24 @@
 
 """
 
+Read a dictionary containing key/value pairs of name:[marks] for a list of students.
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
 
+Sample Input
+3
+Krishna 67 68 69
+Arjun 70 98 63
+Malika 52 56 60
+Malika
+
+Sample Output
+56.00
 
 """
 
 def main():
-
-
+    marks = student_marks[query_name]
+    print(round(sum(marks)/len(marks),2))
 
 if __name__ == '__main__':
     n = int(input())
@@ -18,3 +29,5 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    main()
+

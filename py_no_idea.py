dedfa449406664b59d36a0cd78083adde2be87b8
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-08 07:36:29 -0500

    Initial commit

--- py_no_idea.py
+++ py_no_idea.py
@@ -1,8 +1,17 @@
 #! /bin/python
 
 if __name__ == '__main__':
-    n, m = input().split()
-    N = input().split()
-    A = input().split()
-    B = input().split()
-    print(n,m,N,A,B)
+    n, m = list(map(int, input().split()))
+    N = [int(i) for i in input().split()]
+    A = [int(i) for i in input().split()]
+    B = [int(i) for i in input().split()]
+
+    hapiness = 0
+
+    for _ in range(n):
+        if N[_] in A:
+            hapiness += hapiness
+        if N[_] in B:
+            hapiness -= hapiness
+
+    print(hapiness)

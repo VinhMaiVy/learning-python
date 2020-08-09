b813999e282636c476003940bb1cce264a13e747
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-09 15:27:04 -0500

    Initial commit

---
+++ py_sock_merchant.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+def sockMerchant(n, ar):
+    pairs = 0
+    for element in set(ar):
+        pairs += ar.count(element) // 2
+    return pairs
+
+
+if __name__ == '__main__':
+    n = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = sockMerchant(n, ar)
+    print(result)

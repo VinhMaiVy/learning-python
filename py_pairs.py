7fd6193899d83d97b0aed7394c3e1fd941b27c06
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-10 09:57:56 -0500

    Initial commit

---
+++ py_pairs.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+def pairs(k, arr):
+    return
+
+if __name__ == '__main__':
+    nk = input().split()
+    n = int(nk[0])
+    k = int(nk[1])
+    arr = list(map(int, input().rstrip().split()))
+    result = pairs(k, arr)
+    print(result)

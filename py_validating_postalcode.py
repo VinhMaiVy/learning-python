34030762561697bd247ecf4676a3d95e5bdf1e8c
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-11 05:48:51 -0500

    Initial commit

--- py_validating_postalcode.py
+++ py_validating_postalcode.py
@@ -4,8 +4,8 @@ import re
 
 if __name__ == '__main__':
 
-    regex_integer_in_range = r"[1-9][0-9][0-9][0-9][0-9][0-9]"  # Do not delete 'r'.
-    regex_alternating_repetitive_digit_pair = r"_________"  # Do not delete 'r'.
+    regex_integer_in_range = r"^[1-9]\d{5}$"  # Do not delete 'r'.
+    regex_alternating_repetitive_digit_pair = r"(?=(\d)\d\1)"  # Do not delete 'r'.
 
     P = input()
 

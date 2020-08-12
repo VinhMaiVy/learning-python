450fc6b3a31b5ab988b87c0225e5be8a4beec0dd
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-12 07:38:18 -0500

    Initial commit

--- py_moon_journey.py
+++ py_moon_journey.py
@@ -31,16 +31,29 @@
 
 from itertools import combinations as combo
 
-def journeyToMoon2(n, astronaut):
+
+def journeyToMoon2(n, astronaut):                   # Uses set-arithmetic & counting
+
     C = []                                          # partition (as array) of sets of astronauts by country
-    for a,b in astronaut :
+
+    for a,b in astronaut:
         p, m = {a,b}, len(C)                        # p is doubleton set, m is #countries
+
         i = next( (k for k in range(m) if p & C[k]), -1 )
-        if i == -1 : C.append( p ) ; continue       # form a new country
+        if i == -1:
+            C.append( p )
+            continue                                # form a new country
+
         j = next( (k for k in range(i+1,m) if p & C[k]), -1 )
-        if j == -1 : C[i] |= p                      # chain annexation of pair p
-        else : C[i] |= C[j] ; del C[j]              # merge countries along p
-    nC = list(map( len, C )) ; s = n - sum( nC )    # find subtotals
+        if j == -1:
+            C[i] |= p                               # chain annexation of pair p
+        else:
+            C[i] |= C[j]
+            del C[j]                                # merge countries along p
+
+    nC = list(map( len, C ))
+    s = n - sum( nC )                               # find subtotals
+
     return s*(s-1)//2 + s*(n-s) + sum( x*y for x,y in combo(nC,2))
 
 def journeyToMoon(n, astronaut):

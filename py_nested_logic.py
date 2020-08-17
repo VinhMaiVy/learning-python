be79048fde30032928f88a21dbf98ec7ebfcbc63
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-17 07:12:48 -0500

    2020-08-17

--- py_nested_logic.py
+++ py_nested_logic.py
@@ -17,16 +17,16 @@ Nested Logfic
 
 if __name__ == '__main__':
                
-    d2, m2, y2 = list(map(int, input().split()))
-    d1, m1, y1 = list(map(int, input().split()))    
+    d2, m2, y2 = list(map(int, input().split())) # Return date
+    d1, m1, y1 = list(map(int, input().split())) # Due date  
     
     result=0
     
     if y2>y1:
         result = 10000
-    elif m2>m1:
+    elif (y2==y1) and (m2>m1):
         result = 500*(m2-m1)
-    elif d2>d1:
+    elif (y2==y1) and (m2==m1) and (d2>d1):
         result = 15*(d2-d1)
     
     print(result)

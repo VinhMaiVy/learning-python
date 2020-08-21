a6f0388abdd90267044cd2376d3b7d90145594ae
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-20 21:25:37 -0500

    2020-08-20 21:25

--- Problem Solving/py_winning_lottery_ticket.py
+++ Problem Solving/py_winning_lottery_ticket.py
@@ -13,10 +13,36 @@ Input
 Output
 5
 
+set & combination
+bit manipulation
+
 """
 
 def winningLotteryTicket(tickets):
-    pass
+    tickets_d = []
+    tickets_f = 0
+    for t in range(len(tickets)):
+        tb = ''        
+        for n in range(10):
+            if str(n) in tickets[t]:
+                tb += '1'
+            else:
+                tb += '0'
+        tb = int(tb,2)
+        if tb == 1023:
+            tickets_f +=1
+        else:
+            tickets_d.append(tb)
+                
+    # Combination n*(n-1)/2
+    result += int(len(tickets_d)*tickets_f + tickets_f*(tickets_f-1)/2)
+    
+    while len(tickets_d):
+        t1 = tickets_d.pop()
+        for t2 in tickets_d:
+            if t1 | t2 == 1023:
+                result += 1 
+    return result
 
 
 if __name__ == '__main__':
@@ -30,5 +56,5 @@ if __name__ == '__main__':
 
     result = winningLotteryTicket(tickets)
 
-    print(result + '\n')
+    print(result)
     

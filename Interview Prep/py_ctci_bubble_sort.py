1bdf4e746eb8b4e07a9ca073f84e37ce66cf65de
Vinh Nguyen <vinhmaivy@gmail.com>
2020-08-25 10:20:27 -0500

    2020-08-25 10:20

---
+++ Interview Prep/py_ctci_bubble_sort.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+"""
+BubbleSort
+
+Input
+6
+1 2 3 4 5 3
+
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+    len_a= len(a)
+    swapCount = 0
+    for i in range(len_a):
+        for j in range(len_a-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                swapCount += 1
+    print('Array is sorted in ' + str(swapCount) + ' swaps.')    
+    print('First Element:', a[0])
+    print('Last Element:',  a[len_a-1])
+    
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)

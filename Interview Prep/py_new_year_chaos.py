04da10689505a5d87518c743e399995dd77aa042
Vinh Nguyen <vinhmaivy@gmail.com>
2020-09-06 21:18:06 -0500

    2020-09-06 21:18

--- Interview Prep/py_new_year_chaos.py
+++ Interview Prep/py_new_year_chaos.py
@@ -24,30 +24,46 @@ import re
 import sys
 from collections import Counter
 
-def minimumBribes(q):
-    result = 0        
-    len_q = len(q)
-    dic_q = dict()    
-    
-    for n in range(len_q):
-        dic_q[q[n]] = n
-                
-    for n in range(len_q):
-        if q[n]-1>0:
-            if dic_q[q[n]-1] > n:
-                result +=1
-        if q[n]-2>0:
-            if dic_q[q[n]-2] > n:
-                result +=1
-        if q[n]-3>0:
-            if dic_q[q[n]-3] > n:
-                result = 0
-                break    
-    if result:
-        print(result)
-    else:
-        print('Too chaotic')
-                            
+def minimumBribes(q):        
+    result = 0 
+    #
+    # decrease Q by 1 to make index-matching more intuitive
+    # so that our values go from 0 to N-1, just like our
+    # indices.  (Not necessary but makes it easier to
+    # understand.)
+    q = [p-1 for p in q]
+    #
+    # Loop through each person (P) in the queue (Q)
+    for i,p in enumerate(q):
+        # i is the current position of P, while P is the
+        # original position of P.
+        #
+        # First check if any P is more than two ahead of 
+        # its original position
+        if p - i > 2:
+            print("Too chaotic")
+            return
+        #
+        # From here on out, we don't care if P has moved
+        # forwards, it is better to count how many times
+        # P has RECEIVED a bribe, by looking at who is
+        # ahead of P.  P's original position is the value
+        # of P.
+        # Anyone who bribed P cannot get to higher than
+        # one position in front if P's original position,
+        # so we need to look from one position in front
+        # of P's original position to one in front of P's
+        # current position, and see how many of those 
+        # positions in Q contain a number large than P.
+        # In other words we will look from P-1 to i-1,
+        # which in Python is range(P-1,i-1+1), or simply
+        # range(P-1,i).  To make sure we don't try an
+        # index less than zero, replace P-1 with
+        # max(P-1,0)
+        for j in range(max(p-1,0),i):
+            if q[j] > p:
+                result += 1
+    print(result)                           
     
 if __name__ == '__main__':
     t = int(input())

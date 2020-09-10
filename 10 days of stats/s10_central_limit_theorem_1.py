195690f7f0825e19fc6a9a4c4ab29119d157792c
Vinh Nguyen <vinhmaivy@gmail.com>
2020-09-10 10:53:22 -0500

    2020-09-10 11:00

---
+++ 10 days of stats/s10_central_limit_theorem_1.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+"""
+Central Limit Theorem 1
+https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1/tutorial
+
+Input:
+9800
+49
+205
+15
+
+Output:
+
+
+
+"""
+
+
+
+if __name__ == '__main__':
+    
+    max_weight = int(input())
+    nb_boxes = int(input())
+    mean_weight_boxes = int(input())
+    stddev_weight_boxes = int(input())
+    

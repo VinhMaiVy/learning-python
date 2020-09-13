d4ef739bf0a3bcb28f6b5110e13cf7a828098845
Vinh Nguyen <vinhmaivy@gmail.com>
2020-09-13 17:43:35 -0500

    2020-09-13 18:00

--- 10 days of stats/s10_least_square_regression_line.py
+++ 10 days of stats/s10_least_square_regression_line.py
@@ -4,14 +4,30 @@
 Python
 
 Input:
+5
 95 85
 85 95
 80 70
 70 65
 60 70
+80
 
 Output:
+78.288
 
+Input:
+10
+15 10
+12 25
+8 17
+8 11
+7 13
+7 17
+7 20
+6 13
+5 9
+3 15
+10
 
 """
 
@@ -30,18 +46,19 @@ def pearson_coef(x,y):
 
 if __name__ == '__main__':
     
-    x = [0]*5
-    y = [0]*5
-    
-    x[0], y[0] = list(map(float, input().split()))
-    x[1], y[1] = list(map(float, input().split()))
-    x[2], y[2] = list(map(float, input().split()))
-    x[3], y[3] = list(map(float, input().split()))
-    x[4], y[4] = list(map(float, input().split()))
-    
+    n = int(input())
+    x = [0.0]*n
+    y = [0.0]*n
+    for i in range(n):
+        x[i], y[i] = list(map(float, input().rstrip().split()))
+
+    fs_x = float(input())
+            
     b = pearson_coef(x,y) * stddev(y) / stddev(x)
     a = mean(y) - b*mean(x)
-    result = a + b*80.0
+    result = a + b*fs_x
     
-    print("{:.3f}".format( result ))
+    print("a={:.3f}".format( a ))
+    print("b={:.3f}".format( b ))
+    print("fs_y={:.3f}".format( result ))
     

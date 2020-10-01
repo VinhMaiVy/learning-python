79ddf2645b03e04626774b6ff602e5cd24c96f73
Vinh Nguyen <vinhmaivy@gmail.com>
2020-10-01 07:59:12 -0500

    2020-10-01 8:00

---
+++ src/Algorithms/00 Implementation/py_life_the_universe.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+"""
+Python
+
+Input:
+
+Output:
+
+
+"""
+
+if __name__ == '__main__':    
+    
+    while True:
+        n = int(input())
+        if n == 42:
+            break
+        else:
+            print(n)
+    

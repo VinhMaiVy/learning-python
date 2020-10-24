00c6d4c8dee059f97144a106fb637c5eb086224f
Vinh Nguyen <vinhmaivy@gmail.com>
2020-10-24 07:10:25 -0500

    2020-10-24 7:00

--- src/Algorithms/02 Sorting/py_insertion_sort1.py
+++ src/Algorithms/02 Sorting/py_insertion_sort1.py
@@ -11,7 +11,7 @@ Python
 
 Input:
 5
-2 4 6 8 3
+2 4 9 8 1
 
 Output:
 2 4 6 8 8 
@@ -19,12 +19,32 @@ Output:
 2 4 4 6 8 
 2 3 4 6 8
 
+Input:
+5
+1 2 4 5 3
+
+Output:
+1 2 4 5 5
+1 2 4 4 5
+1 2 3 4 5
+
+
 """
 
 
 # Complete the insertionSort1 function below.
 def insertionSort1(n, arr):
-    pass
+    n -= 1
+    stored = arr[n]
+    while n >= 0:
+        if (n == 0) or (arr[n - 1] < stored):
+            arr[n] = stored
+            print(" ".join(map(str, arr)))
+            break
+        else:
+            arr[n] = arr[n - 1]
+            print(" ".join(map(str, arr)))
+        n -= 1
 
 
 if __name__ == '__main__':
